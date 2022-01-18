a3b5442729b302b6a44451311d99fb7e7df4683e
Nhep Nhep <nhep1603@gmail.com>
2022-01-18 14:23:46 +0700

    delete blocprovider

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BlocProvider(
-        create: (context) => CalculateBloc(),
-        child: const Home(),
-      ),
+      home: Home(),
     );
   }
 }
